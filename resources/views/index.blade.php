0a6c13f1f974f3317402ce40ec607b49ebf5c12d
trinh toanh <kythuat.tech5s@gmail.com>
2022-08-15 18:30:51 +0700

    update code

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,7 +11,7 @@
 	<link rel="stylesheet" href="{{Support::asset('theme/frontend/css/style.css')}}">
 	<link rel="stylesheet" href="{{Support::asset('theme/frontend/css/add.css')}}">
 	@yield('css')
-    <script type="text/javascript">
+	<script type="text/javascript">
         var showNotify = "";
         var messageNotify = "{{ Session::get('messageNotify', '') }}";
         var typeNotify = "{{ Session::get('typeNotify', '') }}";
@@ -23,42 +23,58 @@
     {[CMS_HEADER]}
 </head>
 <body>
-    {[CMS_BODY]}
-    @include('header')
-    <section class="content main clearfix">
-        <div class="col-l">
-            @yield('main')
-        </div>
-        @include('base_left_sidebar')
-        @include('base_right_sidebar')
-    </section>
-    @include('footer')
-    {[CMS_FOOTER]}
-    @yield('jsl')
-    <script>
-        function showmnc2(id_mnu2) {
-            if (document.getElementById(id_mnu2).style.visibility == 'visible') {
-                document.getElementById(id_mnu2).style.visibility = 'hidden';
-            } else {
-                document.getElementById(id_mnu2).style.visibility = 'visible';
-            }
-        }
-
-        function showDrawerMenu() {
-            document.querySelector('html').classList.toggle('menu-active');
-            showmnc2("nav-horizontal");
-        }
-        expand = function(itemId) {
-            Array.from(document.getElementsByClassName('menu-c2')).forEach((e, i) => {
-                if (e.id != itemId) e.style.display = 'none'
-            });
-            elm = document.getElementById(itemId);
-            elm.style.display = elm.style.display == 'block' ? 'none' : 'block'
-        }
-    </script>
-    <script src="theme/frontend/js/datepicker.min.js" defer></script>
+	{[CMS_BODY]}
+	@if (isset($contentOnly) && $contentOnly == 1)
+		@yield('main')
+	@else
+		@include('header')
+		<div class="linkway">
+			<div class="main">
+				@yield('breadcrumb')
+			</div>
+		</div>
+		@include('banner_gdns.banner_ads_horizontal',['positionType'=>App\Models\BannerGdnCategory::BANNER_HEADER])
+		<section class="content main clearfix">
+			<div class="col-l">
+				@yield('main')
+			</div>
+			@include('base_left_sidebar')
+			@include('base_right_sidebar')
+		</section>
+		@include('banner_gdns.banner_ads_horizontal',['positionType'=>App\Models\BannerGdnCategory::BANNER_FOOTER])
+		@include('footer')
+	@endif
+	{[CMS_FOOTER]}
+	@include('facebook_base')
+	@include('js_click_quang_cao')
+	@yield('jsl')
+	<script src="theme/frontend/js/toastify.js" defer></script>
+	<script src="theme/frontend/js/base.js" defer></script>
+	<script src="theme/frontend/js/xhr.js" defer></script>
+	<script>
+		function showmnc2(id_mnu2) {
+			if (document.getElementById(id_mnu2).style.visibility == 'visible') {
+				document.getElementById(id_mnu2).style.visibility = 'hidden';
+			} else {
+				document.getElementById(id_mnu2).style.visibility = 'visible';
+			}
+		}
+		function showDrawerMenu() {
+			document.querySelector('html').classList.toggle('menu-active');
+			showmnc2("nav-horizontal");
+		}
+		expand = function(itemId) {
+			Array.from(document.getElementsByClassName('menu-c2')).forEach((e, i) => {
+				if (e.id != itemId) e.style.display = 'none'
+			});
+			elm = document.getElementById(itemId);
+			elm.style.display = elm.style.display == 'block' ? 'none' : 'block'
+		}
+	</script>
+	<script src="theme/frontend/js/datepicker.min.js" defer></script>
     <script src="theme/frontend/js/vi.js" defer></script>
     <script src="theme/frontend/js/main.js" defer></script>
-    @yield('js')
+	@yield('js')
 </body>
+
 </html>
